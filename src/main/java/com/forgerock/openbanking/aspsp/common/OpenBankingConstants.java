365fd97de200257135863070c816aba4f74f3a62
Matt Wills <matt.wills@forgerock.com>
2020-11-17 12:53:14 +0000

    Added services and common utilities

---
+++ src/main/java/com/forgerock/openbanking/aspsp/common/OpenBankingConstants.java
@@ -0,0 +1,12 @@
+package com.forgerock.openbanking.aspsp.common;
+
+public class OpenBankingConstants {
+    public static final String HTTP_DATE_FORMAT = "EEE, dd MMM yyyy HH:mm:ss z";
+    public static final String STATEMENT_TIME_DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss";
+    public static final String AVAILABLE_DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ssZZ";
+    public static final String BOOKED_TIME_DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss";
+    public static final String FROM_BOOKING_DATE_TIME = "fromBookingDateTime";
+    public static final String TO_BOOKING_DATE_TIME = "toBookingDateTime";
+    public static final String FROM_STATEMENT_DATE_TIME = "fromStatementDateTime";
+    public static final String TO_STATEMENT_DATE_TIME = "toStatementDateTime";
+}
